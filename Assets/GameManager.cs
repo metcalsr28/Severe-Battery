d54f22bf37be01e2f5dad9d7c99da35b32aff622
metcalsr28 <metcalsr@gmail.com>
2021-06-15 21:36:20 -0400

    Moving Directories

---
+++ Assets/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Score score;
+
+    void GameOver()
+    {
+        Debug.Log("GAME OVER");
+    }
+}
